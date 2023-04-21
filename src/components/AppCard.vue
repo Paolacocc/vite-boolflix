fd9b370ba08880c3930160d9966eb57d652e98d1
Paolacocc <coccettipaola@gmail.com>
2023-04-21 11:53:48 +0200

    add basic graphic

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -14,90 +14,109 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col">
+    <div class="card-bkg pt-4">
+        <div class="container">
+            <div class="row">
+                <h2 class="p-4">MOVIES</h2>
+                <div class="ms_col col-movie">
 
 
-                <!-- /CARD-MOVIE -->
-                <div class="card card-movie" v-for="card in store.movies">
-                    <img :src="`${store.imgPath}${card.poster_path}`" alt="" v-if="card.poster_path">
-                    <div v-else>sorry no image</div>
-                    <h3>{{ card.title }}</h3>
-                    <h4>{{ card.original_title }}</h4>
-                    <h4>
-                        <img src="../assets/img/Flag_of_Italy_Flat_Wavy-64x49.png" alt=""
-                            v-if="card.original_language === 'it'">
-                        <img v-else-if="card.original_language === 'en'"
-                            src="../assets/img/Flag_of_United_Kingdom_Flat_Wavy-64x37.png" alt="">
-                        <img v-else-if="card.original_language === 'fr'"
-                            src="../assets/img/Flag_of_France_Flat_Wavy-64x49.png" alt="">
-                        <h4 v-else>{{ card.original_language }}</h4>
-                    </h4>
+                    <!-- /CARD-MOVIE -->
 
-                    <h4>
-                        <span v-for="i in store.stars">
-                            <i
-                                :class="[(i <= Math.floor(card.vote_average / 2)) ? 'fa-solid fa-star' : 'fa-regular fa-star']"></i>
-                        </span>
-                    </h4>
+                    <div class="_ms-card card-movie" v-for="card in store.movies">
+                        <img :src="`${store.imgPath}${card.poster_path}`" alt="" v-if="card.poster_path">
+                        <div v-else>sorry no image</div>
+                        <h3>{{ card.title }}</h3>
+                        <h4>{{ card.original_title }}</h4>
+                        <h4>
+                            <img class="img-flag" src="../assets/img/Flag_of_Italy_Flat_Wavy-64x49.png" alt=""
+                                v-if="card.original_language === 'it'">
+                            <img class="img-flag" v-else-if="card.original_language === 'en'"
+                                src="../assets/img/Flag_of_United_Kingdom_Flat_Wavy-64x37.png" alt="">
+                            <img class="img-flag" v-else-if="card.original_language === 'fr'"
+                                src="../assets/img/Flag_of_France_Flat_Wavy-64x49.png" alt="">
+                            <h4 v-else>{{ card.original_language }}</h4>
+                        </h4>
+
+                        <h4>
+                            <span v-for="i in store.stars">
+                                <i
+                                    :class="[(i <= Math.floor(card.vote_average / 2)) ? 'fa-solid fa-star' : 'fa-regular fa-star']"></i>
+                            </span>
+                        </h4>
+                    </div>
                 </div>
+                </div>
+
                 <!-- /CARD-MOVIE -->
 
 
                 <!-- CARD-TV-SERIE -->
-                <div class="card card-tv" v-for="card in store.tvSeries">
-                    <img :src="`${store.imgPath}${card.poster_path}`" alt="" v-if="card.poster_path">
-                    <div v-else>sorry no image</div>
-                    <h3>{{ card.name }}</h3>
-                    <h4>{{ card.original_name }}</h4>
-                    <h4>
-                        <img src="../assets/img/Flag_of_Italy_Flat_Wavy-64x49.png" alt=""
-                            v-if="card.original_language === 'it'">
-                        <img v-else-if="card.original_language === 'en'"
-                            src="../assets/img/Flag_of_United_Kingdom_Flat_Wavy-64x37.png" alt="">
-                        <img v-else-if="card.original_language === 'fr'"
-                            src="../assets/img/Flag_of_France_Flat_Wavy-64x49.png" alt="">
-                        <h4 v-else>{{ card.original_language }}</h4>
-                    </h4>
+                <div class="row">
+                    <h2 class="p-4">TV SERIES</h2>
+                    <div class="ms_col col-tv">
 
-                    <h4>
-                        <span v-for="star in store.stars">
-                            <i
-                                :class="[(star <= Math.floor(card.vote_average / 2)) ? 'fa-solid fa-star' : 'fa-regular fa-star']"></i>
-                        </span>
-                    </h4>
+                        <div class="_ms-card card-tv " v-for="card in store.tvSeries">
+                            <img :src="`${store.imgPath}${card.poster_path}`" alt="" v-if="card.poster_path">
+                            <div v-else>sorry no image</div>
+                            <h3>{{ card.name }}</h3>
+                            <h4>{{ card.original_name }}</h4>
+                            <h4>
+                                <img class="img-flag" src="../assets/img/Flag_of_Italy_Flat_Wavy-64x49.png" alt=""
+                                    v-if="card.original_language === 'it'">
+                                <img class="img-flag" v-else-if="card.original_language === 'en'"
+                                    src="../assets/img/Flag_of_United_Kingdom_Flat_Wavy-64x37.png" alt="">
+                                <img class="img-flag" v-else-if="card.original_language === 'fr'"
+                                    src="../assets/img/Flag_of_France_Flat_Wavy-64x49.png" alt="">
+                                <h4 v-else>{{ card.original_language }}</h4>
+                            </h4>
+
+                            <h4>
+                                <span v-for="star in store.stars">
+                                    <i
+                                        :class="[(star <= Math.ceil(card.vote_average / 2)) ? 'fa-solid fa-star' : 'fa-regular fa-star']"></i>
+                                </span>
+                            </h4>
+                        </div>
+
+                        <!-- /CARD-TV-SERIE -->
+
+                    </div>
                 </div>
-                <!-- /CARD-TV-SERIE -->
-
-            </div>
+           
         </div>
     </div>
 </template>
 
 <style scoped>
-.col {
+.card-bkg {
+    background-color: black;
+    height: calc(100vh - 80px);
+    overflow-y: auto;
+
+}
+
+.ms_col {
 
     display: flex;
-    flex-wrap: wrap;
+    overflow-x: auto;
+    gap: 5px;
 
 }
 
-.card {
+._ms-card {
     text-align: center;
-    width: calc(100% / 6 - 10px);
-    height: 200px;
-    margin: 5px;
+    width: 200px;
+    height: 300px;
     padding: 10px;
 
+    border-color: white;
+    background-color: darkgray;
 }
 
-.card-movie {
-    background-color: lightpink;
-}
 
-.card-tv {
-    background-color: lightblue;
+h2 {
+    color: grey;
 }
 
 h3 {
@@ -107,8 +126,11 @@ h3 {
 h4 {
     font-size: .8rem;
 }
-
 img {
+    max-width: 100%;
+}
+
+.img-flag {
     width: 30px;
 }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,7 @@ export default {
     data() {
         return {
             store,
-            
+
 
         }
     }
@@ -15,10 +15,32 @@ export default {
 </script>
 
 <template>
-    <div class="header text-center">
-    <h1>HEADER</h1>
-    <label for="search"></label>
-    <input type="text" id="search" v-model.trim="store.writtenText" @keyup.enter="$emit('filter')">
+    <div class="header">
+    <div class="container">
+    <div class="d-flex justify-content-between align-items-center p-4">
+        <img src="../assets/img/BOOLFLIX.png" alt="">
+        <div class="serch-bar ">
+            <label for="search"></label>
+            <input type="text" id="search" v-model.trim="store.writtenText" >
+            <button class="btn-red" @click="$emit('filter')">INVIA</button>
+        </div>
+    </div>
+    </div>
 </div>
-    
 </template>
+
+<style scoped lang="scss">
+.header {
+   background-color: black;
+   height: 80px;
+   .btn-red {
+    border: none;
+    background-color: red;
+    color: white;
+    border-radius: 5px;
+    margin-left: 10px;
+    padding: 3px 10px;
+   }
+
+}
+</style>
